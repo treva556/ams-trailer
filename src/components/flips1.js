c4195a09dbc228e3ceb20cf2a566945cee562eea
treva556 <marktreva92@gmail.com>
2024-07-12 23:00:31 +0300

    start

---
+++ src/components/flips1.js
@@ -0,0 +1,42 @@
+
+
+
+import React, { useState, useEffect } from 'react';
+
+const Flippings = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const contents = [
+    "Content 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "Content 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "Content 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "Content 4. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "Content 5. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "Content 6. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 3) % contents.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [contents.length]);
+
+  return (<div className=' mt-5'>
+    <p className=' text-lg font-bold mb-3'>  Our Various Clients </p>
+    <div className="columns-3 gap-4">
+      {contents.slice(currentIndex, currentIndex + 3).map((content, index) => (
+        <div
+          key={index}
+          className="p-4 bg-gray-200 rounded shadow-md transition-opacity duration-500 ease-in-out"
+        >
+          {content}
+        </div>
+      ))}
+    </div>
+    </div>
+  );
+};
+
+export default Flippings;
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import '../App.css';
+import FlippingContent from '../components/flips';
+import Flippings from '../components/flips1';
 
 function Home() {
   return (
@@ -12,6 +14,19 @@ function Home() {
      <div className=' bg-yellow-300 p-4'>
         jjj
      </div>
+     <div class="flex flex-col md:flex-row">
+        <div class="p-4 bg-blue-500">Item 1</div>
+        <div class="p-4 bg-green-500">Item 2</div>
+        <div class="p-4 bg-red-500">Item 3</div>
+        </div>
+        <div class="columns-3">
+        <p>Content 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p>Content 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p>Content 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <Flippings/>
+        <FlippingContent/>
+
     </div>
   );
 }
