c2dedd916abe8ef53bce181dfd856f9cc8376cd0
treva556 <marktreva92@gmail.com>
2024-07-13 17:52:52 +0300

    start

--- src/components/flips.js
+++ src/components/flips.js
@@ -23,16 +23,16 @@ const FlippingContent = () => {
   }, [contents.length]);
 
   return (
-    <div>
-      <p className='mt-3 text-xl font-bold text-black'>Our Various Clients</p>
+    <div className=' bg-slate-200'>
+      <p className='mt-3 text-xl font-bold text-black mb-3'>Our Various Clients</p>
       <div className="columns-3 gap-4">
         {Array.from({ length: 3 }).map((_, idx) => {
           const contentIndex = (currentIndex + idx) % contents.length;
           return (
             <div
               key={idx}
-              className={`p-4 rounded shadow-md transition-opacity duration-500 ease-in-out ${
-                contentIndex % 2 === 0 ? ' text-black' : 'bg-yellow-300 text-black'
+              className={`p-4 rounded mb-4 shadow-md transition-opacity duration-500 ease-in-out ${
+                contentIndex % 2 === 0 ? ' bg-white text-black' : 'bg-yellow-300 text-black'
               }`}
             >
               {contents[contentIndex]}
--- src/pages/home.js
+++ src/pages/home.js
@@ -10,26 +10,48 @@ import 'typeface-jetbrains-mono';
 function Home() {
   return (
     <div className="App">
+        <div className='bg-slate-300 p-4'>
+            hhh
+        </div>
      <div className='ms p-32 bg-slate-700'>
          HEADER
      </div>
-     <p className=' font-bold'> Archangel Michael Security Firm</p>
+     <p className=' p-4 bg-slate-200 font-bold'> Archangel Michael Security Firm</p>
+
+     <div class="flex flex-col md:flex-row">
+
      <div className=' bg-yellow-300 p-4'>
-        <p> About AMS </p>
+        <p className=' font-bold'> About AMS </p>
         <p className=' ms'> AMS Security was founded by the church who saw a need for high-quality security services in the local community. Starting with a small team of dedicated professionals, the company has grown over the years to become a leading provider of manned security solutions. Our journey is marked by numerous milestones, including expanding our service areas, acquiring advanced security technology, and receiving multiple industry awards for excellence. </p>
      </div>
-     <div class="flex flex-col md:flex-row">
-        <div class="p-4 bg-blue-500">mission & values</div>
-        <div class="p-4 ">
-            services
-            <p className=' ms'> AMS Security was founded by the church who saw a need for high-quality security services in the local community. Starting with a small team of dedicated professionals, the company has grown over the years to become a leading provider of manned security solutions. Our journey is marked by numerous milestones, including expanding our service areas, acquiring advanced security technology, and receiving multiple industry awards for excellence. </p>
-
-            </div>
-        <div class="p-4 bg-yellow-300 ">
+        {/* <div class="p-4 bg-blue-500">mission & values</div> */}
+        
+        <div class="p-4 bg-gray-400">
+            <p className=' font-bold'> Mission & Values</p>
         <p className=' ms'> AMS Security was founded by the church who saw a need for high-quality security services in the local community. Starting with a small team of dedicated professionals, the company has grown over the years to become a leading provider of manned security solutions. Our journey is marked by numerous milestones, including expanding our service areas, acquiring advanced security technology, and receiving multiple industry awards for excellence. </p>
 
         </div>
+
         </div>
+        <div className=' font-bold mt-4'>
+        services
+
+        <div class="flex flex-col md:flex-row ml-2 mr-2 ">
+
+        <div class="p-4 ">
+            <p className=' ms'> 1. AMS Security was founded by the church who saw a need for high-quality security services in the local community. Starting with a small team of dedicated professionals, the company has grown over the years to become a leading provider of manned security solutions. Our journey is marked by numerous milestones, including expanding our service areas, acquiring advanced security technology, and receiving multiple industry awards for excellence. </p>
+
+            </div>
+            <div>
+            <p className=' ms bg-black text-white'> 2.  AMS Security was founded by the church who saw a need for high-quality security services in the local community. Starting with a small team of dedicated professionals, the company has grown over the years to become a leading provider of manned security solutions. Our journey is marked by numerous milestones, including expanding our service areas, acquiring advanced security technology, and receiving multiple industry awards for excellence. </p>
+
+            </div>
+          </div>
+        </div>
+
+        <div>
+        <p className=' font-bold mt-4'> Variety contents</p>
+
         <div class=" mt-3 columns-3">
             <div className='border-t-4 border-indigo-500 '>
         <p> We deal in events like weddings and fundraisers... </p>
@@ -37,16 +59,16 @@ function Home() {
         <p>Content 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         <p>Content 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
-        <div className=' font-bold' >
-        <div >
-            training videos
         </div>
+        <div className=' mt-5 mb-5 font-bold' >
+            <div >
+                training videos
+            </div>
 
-        <div>
-            other images
-        </div>
-        </div>
-
+            <div>
+                other images
+            </div>
+        </div >
         <FlippingContent/>
 
         <Flippings/>
