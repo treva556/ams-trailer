a9cf394725ac6695ae5354e3f6da2d2cff6b3d24
treva556 <marktreva92@gmail.com>
2024-09-30 23:03:16 +0300

    milestone 3

--- src/components/flips1.js
+++ src/components/flips1.js
@@ -1,23 +1,29 @@
 
-
 import React, { useState, useEffect } from 'react';
+import '../App.css'; 
 
 const Flippings = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [fade, setFade] = useState(false);
 
   const contents = [
     "Their guards are courteous and well-trained. They have exceeded our expectations in terms of reliability and customer service. Highly recommend!",
     "Responsive, reliable, and always ready to help. Their guards have been instrumental in safeguarding our premises, and we couldn’t be happier.",
     "Exceptional service and professionalism. The security team was always on time, well-trained, and attentive to detail. We feel much safer with their presence.",
     "Their professionalism and dedication were evident from day one. We’ve had zero issues since hiring them, and I can’t recommend them enough.", 
-    "Exceptional service and professionalism. The security team was always on time, well-trained, and attentive to detail. We feel much safer with their presence.",
     "We’re very satisfied with AMS. Their security personnel are well-trained, friendly, and highly capable in handling any situation." ,
+    "Responsive, reliable, and always ready to help. Their guards have been instrumental in safeguarding our premises, and we couldn’t be happier.",
   ];
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 3) % contents.length);
-    }, 5000);
+      setFade(false); 
+
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 3) % contents.length);
+        setFade(true); // Trigger fade-in
+      }, 900); 
+    }, 8000);
 
     return () => clearInterval(interval);
   }, [contents.length]);
@@ -26,13 +32,13 @@ const Flippings = () => {
     <div className='mt-5 mb-4'> 
       <p className='text-lg font-bold mb-3'>What Our Clients Say</p>
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 bg-slate-200"> {/* Use grid instead of columns */}
+      <div className={`grid grid-cols-1 lg:grid-cols-3 gap-4 bg-slate-200 transition-opacity duration-500 ${fade ? 'fade-show' : 'fade'}`}> 
         {contents.slice(currentIndex, currentIndex + 3).map((content, index) => (
           <div
             key={index}
             className="p-6 rounded shadow-md bg-white m-0" 
           >
-           " {content}"
+            " {content}"
           </div>
         ))}
       </div>
@@ -41,8 +47,3 @@ const Flippings = () => {
 };
 
 export default Flippings;
-
-
-
-
-
