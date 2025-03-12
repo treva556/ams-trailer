051d906002bd309b4a87d72af7bb501c698c26c4
treva556 <marktreva92@gmail.com>
2025-03-12 22:06:30 +0300

    done

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -7,8 +7,6 @@ import { Link } from 'react-router-dom';
 function Careers() {
   return (
     <div className="App">
-      
-
       <div className='flex justify-between px-8 lg:px-36 p-4 m-2 rounded-md bg-slate-100 border '>
         <h1 className='text-2xl'> Training Officers </h1>
         <button className="bg-transparent hover:bg-black text-black font-semibold hover:text-yellow-300 py-2 px-4 border border-black hover:border-transparent rounded">
@@ -31,12 +29,18 @@ function Careers() {
           <h1 className='text-xl lg:text-2xl'>Accountant</h1>
           <p className='text-sm lg:text-left text-start'>We are looking for a skilled-experiences accountant to join our team.</p>
         </div>
-        <div className='flex flex-col lg:flex-row justify-between px-1 lg:px-36 p-4 border border-white border-y-black m-2'>
+        
+      
+        <button className="self-start bg-transparent hover:bg-black text-black font-semibold hover:text-yellow-300 py-1 px-1 border border-black hover:border-transparent rounded w-1/2 lg:w-auto mt-7 lg:mt-0">
+          <Link to="/apply">Check Out!</Link>
+        </button>
+      </div>
+
+      <div className='flex flex-col lg:flex-row justify-between px-1 lg:px-36 p-4 border border-white border-y-black m-2'>
         <div className='flex flex-col items-start justify-start'>
           <h1 className='text-xl lg:text-2xl'>Security Guards</h1>
           <p className='text-sm lg:text-left text-start'>We are looking for a skilled and dedicated candidate for a security guard position.</p>
         </div>
-        
         <button className="self-start bg-transparent hover:bg-black text-black font-semibold hover:text-yellow-300 py-1 px-1 border border-black hover:border-transparent rounded w-1/2 lg:w-auto mt-7 lg:mt-0">
           <Link to="/apply">Check Out!</Link>
         </button>
